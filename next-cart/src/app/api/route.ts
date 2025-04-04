c1a6a946c37c30fd8dd7f115617e8bb73e523911
Adebumiti Olusegun A. <39396698+oluwin@users.noreply.github.com>
2025-04-04 21:10:25 +0100

    Deleted route.ts

--- next-cart/src/app/api/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import {auth} from "@/components/auth/auth";
-
-export const GET = auth(async (req: { auth: any; }) => {
-    if (!req.auth) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-
-    return NextResponse.json({ message: "Authenticated content" });
-});
